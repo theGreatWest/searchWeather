0b577a4457d37eb9e601ffaa14f979c60d9505a1
youngjuKim <kjuzoojuk@naver.com>
2025-02-01 17:39:41 +0900

    chore: 웹 프로젝트 초기 설정

---
+++ src/main/java/com/weather/searchWeather/SearchWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.weather.searchWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/searchWeather/SearchWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.searchWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
